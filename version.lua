4efe1d4df86d02a33648b8161d1859cf084702b5
ry <75577692+ry361@users.noreply.github.com>
2026-05-11 13:56:29 +0300

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,6 +1,6 @@
 return {
     vendingmachine = {
-        version = "1.0.0",
+        version = "1.0.1",
         releaseDate = "2026-05-11",
         critical = false,
         download = "https://portal.cfx.re/assets/granted-assets",
